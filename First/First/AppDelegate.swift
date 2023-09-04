a79facf3f8f293a04750bf1eb44ded176b6207f5
ryu <munam4@naver.com>
2023-09-04 21:40:03 +0900

    폴더 정리, iOS 프로젝트 생성

---
+++ First/First/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  First
+//
+//  Created by ryu hyunsun on 2023/09/04.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // UI code 작성
+        print("launch app")
+        return true
+    }
+}
