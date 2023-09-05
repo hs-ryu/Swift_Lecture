db186986a3fa2f0a1004c016ce0bc35aea5f161c
ryu <munam4@naver.com>
2023-09-05 21:49:54 +0900

    230905 강의 정리

--- First/First/AppDelegate.swift
+++ First/First/AppDelegate.swift
@@ -8,11 +8,17 @@
 import Foundation
 import UIKit
 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.gray
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
         // UI code 작성
-        print("launch app")
         return true
     }
 }
