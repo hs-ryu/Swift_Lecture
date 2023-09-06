b72b9ad96834a229bd646c4691c5e13d81b35cbf
ryu <munam4@naver.com>
2023-09-06 21:53:22 +0900

    230906 강의 정리

--- First/First/AppDelegate.swift
+++ First/First/AppDelegate.swift
@@ -16,9 +16,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow()
         window?.backgroundColor = UIColor.gray
-        window?.rootViewController = UIViewController()
+        
+        // View를 만들어 window 안에 넣기
+        let rc = CGRect(x:100, y:200, width: 200, height: 400)
+        let view = UIView(frame: rc)
+        view.backgroundColor = UIColor.yellow
+        window?.addSubview(view)
+
+        // button을 만들어 View 안에 넣기
+        let btn = UIButton(type:UIButton.ButtonType.roundedRect)
+        // let bt = UIButton(type: .roundedRect)
+        btn.frame = CGRect(x:50, y:100, width: 100, height: 100)
+        btn.backgroundColor = UIColor.green
+        view.addSubview(btn)
+        
+        let vc = FirstViewController()
+        vc.view = view
+        
+        
+        
+        window?.rootViewController = vc // vc.view를 윈도우에 자동으로 붙여줌.
+        // 윈도우를 보이게 해라!
         window?.makeKeyAndVisible()
         // UI code 작성
+        
+        btn.addTarget(vc, action: #selector(FirstViewController.buttonClick), for: .touchUpInside)
+        
         return true
     }
 }
---
+++ First/First/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  First
+//
+//  Created by ryu hyunsun on 2023/09/06.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClick() {
+        print("button click")
+    }
+}
