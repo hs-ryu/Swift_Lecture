321c31a7ceb3c0b510da3ee4b075ace876722ab4
ryu <munam4@naver.com>
2024-02-07 21:52:13 +0900

    패스트 캠퍼스 강의 정리

--- FastCampus 강의/Spotify 스타일 로그인 앱/SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ FastCampus 강의/Spotify 스타일 로그인 앱/SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -6,13 +6,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController:UIViewController {
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var errorTextLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
     
@@ -33,8 +31,47 @@ class EnterEmailViewController:UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
+        // 파이어베이스 이메일, 비밀번호 인증.
+        // 만약 빈값이라면 빈값 처리. 옵셔널 처리.
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
         
+        // 신규 사용자 생성.
+        Auth.auth().createUser(withEmail: email, password: password) {[weak self] authResult, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007:
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorTextLabel.text = error.localizedDescription
+                }
+            } else {
+                self.showMainViewController()
+            }
+        }
     }
+    
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in
+            guard let self = self else { return }
+            if let error = error {
+                errorTextLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
+    }
+    
 }
 
 extension EnterEmailViewController: UITextFieldDelegate {
--- FastCampus 강의/Spotify 스타일 로그인 앱/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ FastCampus 강의/Spotify 스타일 로그인 앱/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
@@ -22,11 +23,22 @@ class MainViewController: UIViewController {
         navigationController?.navigationBar.isHidden = true
         
         
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다
+        \(email)님
+        """
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
         
-        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("ERROR : SignOut \(signOutError.localizedDescription)")
+        }
     }
 }
